0ecf67fe0867947347342c887db357f7718fb296
Fred Maya <archonsyndicate@gmail.com>
2023-11-19 19:10:28 +0100

    tweak tag and icon size

--- src/components/solar-ds/solar-tag/tag.tsx
+++ src/components/solar-ds/solar-tag/tag.tsx
@@ -26,8 +26,8 @@ export class SolarTag {
           <slot />
           {this.isActive && this.icon && 
           <svg class="icon"  width="24" height="24" viewBox="0 0 24 24" fill="none">
-            <path d="M4.97997 18.9966L19.0294 5.03613" stroke="var(--icon-color)" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M19.0295 18.9966L4.98004 5.03613" stroke="var(--icon-color)" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M4.97997 18.9966L19.0294 5.03613" stroke="var(--icon-color)" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M19.0295 18.9966L4.98004 5.03613" stroke="var(--icon-color)" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>}
         </span>
 
